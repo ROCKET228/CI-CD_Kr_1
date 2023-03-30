c6abfcba0a1550565847173d76345811e4de8aa4
ROCKET228 <michkahap@gmail.com>
2023-03-30 14:20:34 +0300

    Create fileRead def

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
     filename = "Countries.txt"
+    country_populations = fileRead(filename)
+
+def fileRead(filename):
+    country_populations = {}
+
+    with open(filename, 'r') as file:
+        for line in file:
+            country, year, population = line.strip().split(", ")
+            population = int(population)
+
+            if country in country_populations:
+                country_populations[country].append((year, population))
+            else:
+                country_populations[country] = [(year, population)]
+
+    return country_populations
